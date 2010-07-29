9eeafa8f7fa5635d6698ff1e6cd7d25073ba6140
elgarat <elgarat@u9.ru>
2010-07-29 16:44:15 +0400

    правки в news.ui.php

--- kernel/var/ui/news/news.ui.php
+++ kernel/var/ui/news/news.ui.php
@@ -12,20 +12,25 @@ class ui_news extends user_interface
 {
 	public $title = 'Новости';
 	
-	public function __construct ()
+	public function __construct()
 	{
-		parent::__construct(__CLASS__);
+		parent::__construct((func_num_args() > 0) ? func_get_arg(0) : __CLASS__);
+		$this->files_path = dirname(__FILE__).'/'; 
 	}
         
-        /**
+	/**
         *       Отрисовка контента для внешней части
         */
         public function pub_content()
         {
-		$tmpl = new tmpl($this->pwd() . 'content.html');
+		/*$tmpl = new tmpl($this->pwd() . 'content.html');*/
                 $di = data_interface::get_instance('news');
 		$di->set_args($this->args);
-                return $tmpl->parse($di->_get());
+//                return $tmpl->parse($di->_get());
+//		$data = array();
+		$data['news'] = $di->_get();
+//		dbg::show($data);
+		return $this->parse_tmpl('default.html',$data);
         }
 	
 	/**
@@ -36,5 +41,13 @@ class ui_news extends user_interface
 		$tmpl = new tmpl($this->pwd() . 'news.js');
 		response::send($tmpl->parse($this), 'js');
 	}
+
+
+	public function pub_double()
+        {
+		return 'guest book here';
+	}
+
+
 }
 ?>
