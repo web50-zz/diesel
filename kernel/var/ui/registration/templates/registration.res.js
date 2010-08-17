055fb1f5ebbfa9c81f9536c6e50598a2daf89290
Fedot B Pozdnyakov 9* <9@u9.ru>
2010-08-17 22:57:54 +0400

    registration UI - spinner icon added due servere request. Css slightly changed

--- kernel/var/ui/registration/templates/registration.res.js
+++ kernel/var/ui/registration/templates/registration.res.js
@@ -2,7 +2,7 @@ Ext.namespace("ui.registration");
 
 ui.registration = function(conf){
 
-this.collectButtons = function(){
+	this.collectButtons = function(){
 		Ext.each(Ext.query(".submbutt"), function(item, index, allItems){
 			Ext.get(item).on({
 				click: function(ev, el, opt){
@@ -12,63 +12,77 @@ this.collectButtons = function(){
 			})
 		}, this);
 
-}
+	}
 
-this.handleSubmit = function(){
-	Ext.each(Ext.query(".req",Ext.fly('.regform')), function(item, index, allItems){
-		el = Ext.get(item);
-		if(el.getValue() == '')
-		{
-			var elt = Ext.fly(el.getAttribute('fldttlid'));
-			el.replaceClass('field','field_error');
-			elt.replaceClass('field_name','field_name_error');
-		}
-		else
-		{
-			var elt = Ext.fly(el.getAttribute('fldttlid'));
-			el.replaceClass('field_error','field');
-			elt.replaceClass('field_name_error','field_name');
-		}
-	}, this);
-	Ext.Ajax.request({
-		url: '/ui/registration/register.do',
-		form: 'regform',
-		success: function(response, opts) {
-		var obj = Ext.decode(response.responseText);
-			if(obj.code == '400')
+	this.handleSubmit = function(){
+		Ext.each(Ext.query(".req",Ext.fly('.regform')), function(item, index, allItems){
+			el = Ext.get(item);
+			if(el.getValue() == '')
 			{
-				Ext.fly('errortext').dom.innerHTML = obj.error;
+				var elt = Ext.fly(el.getAttribute('fldttlid'));
+				el.replaceClass('field','field_error');
+				elt.replaceClass('field_name','field_name_error');
 			}
 			else
 			{
-				Ext.fly('errortext').dom.innerHTML = '';
+				var elt = Ext.fly(el.getAttribute('fldttlid'));
+				el.replaceClass('field_error','field');
+				elt.replaceClass('field_name_error','field_name');
 			}
-
-			if(obj.code == '200')
-			{
-				Ext.fly('report').dom.innerHTML = obj.report;
-				Ext.Ajax.request({
-					url: '?',
-					success: function(response,opts){
-							window.location="?";
-						},
-					failure: function(response,opts){
-							alert('failure');
-						},
-					params: { user: Ext.fly('email').getValue(),secret: Ext.fly('passwd').getValue()}
-				});
-			}
-			else
-			{
-				Ext.fly('report').dom.innerHTML = '';
-			}
-		},
-		 failure: function(response, opts) {
-			 console.log(' Error ' + response.status);
+		}, this);
+		Ext.Ajax.on('beforerequest', this.showSpinner, this);
+		Ext.Ajax.on('requestcomplete', this.hideSpinner, this);
+		Ext.Ajax.on('requestexception', this.hideSpinner, this);
+		Ext.Ajax.request({
+			url: '/ui/registration/register.do',
+			form: 'regform',
+			success: function(response, opts) {
+				var obj = Ext.decode(response.responseText);
+				if(obj.code == '400')
+				{
+					Ext.fly('errortext').dom.innerHTML = obj.error;
+				}
+				else
+				{
+					Ext.fly('errortext').dom.innerHTML = '';
+				}
+				if(obj.code == '200')
+				{
+					Ext.fly('report').dom.innerHTML = obj.report;
+					//window.location="?secret="+Ext.fly('passwd').getValue()+"&user="+Ext.fly('email').getValue();
+					Ext.Ajax.request({
+						url: '?',
+						success: function(response2,opts){
+								window.location="";
+							},
+						failure: function(response2,opts){
+								alert('failure');
+							},
+						params: {user: Ext.fly('email').getValue(),secret: Ext.fly('passwd').getValue()}
+					});
+				}
+				else
+				{
+					Ext.fly('report').dom.innerHTML = '';
+				}
+			},
+			 failure: function(response, opts) {
+					 console.log(' Error ' + response.status);
 			}
 		});
 	}
 	
+	this.showSpinner =  function(){
+		Ext.fly('registr').insertFirst({
+		tag: 'div',
+		cls: 'spinner',
+		id: 'spinner',
+		html: 'cоединение'
+		});
+	}
+	this.hideSpinner =  function(){
+		Ext.fly('spinner').remove();
+	}
 }
 
 Ext.onReady(function(){
