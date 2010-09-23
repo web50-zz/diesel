71efd39c6955034e251d34124ea4d0f5d034e4c7
Fedot B Pozdnyakov 9* <9@u9.ru>
2010-09-23 21:19:43 +0400

    simple market catalogue navigation added

---
+++ kernel/var/ui/market_cat_nav/market_cat_nav.ui.php
@@ -0,0 +1,36 @@
+<?php
+/**
+*	UI simple menu for market catalogue
+*
+* @author	9* <9@u9.ru>
+* @access	public
+* @package	SBIN Diesel
+*/
+class ui_market_cat_nav extends user_interface
+{
+	public $title = 'Меню по типам товаров в каталоге';
+
+	
+	public function __construct()
+	{
+		parent::__construct((func_num_args() > 0) ? func_get_arg(0) : __CLASS__);
+		$this->files_path = dirname(__FILE__).'/'; 
+	}
+
+	/**
+	*	main menu
+	*/
+	protected function pub_top_menu()
+	{
+		$st = data_interface::get_instance('guide_type');
+		// 9* detects type if already in catalogue to highlite selected
+
+		$data = $st->extjs_grid_json(false,false);
+		if (preg_match('/type\/(\d+)\//', SRCH_URI, $matches)&&PAGE_URI == '/products/')
+		{
+			$data['toselect'] = $matches[1];
+		}
+		return $this->parse_tmpl('main_menu.html',$data);
+	}
+}
+?>
