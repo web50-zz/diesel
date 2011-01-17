e7a22673ba49c13540356ce7c41dee17944f5fbe
Fedot B Pozdnyakov 9* <9@u9.ru>
2011-01-17 21:46:24 +0300

    new theme wfor web50

---
+++ themes/web50ru/js_deps.php
@@ -0,0 +1,10 @@
+<?
+$js_deps = array(
+	'js/jquery.js',
+	'js/curvycorners.src.js',
+	'js/cufon-yui.js',
+	'js/cl-reg_400.font.js',
+	'js/cl-reg_it_italic_400.font.js',
+	'js/slide.js',
+);
+?>
---
+++ themes/web50ru/tmpl/structure/structure.res.js
@@ -0,0 +1,5 @@
+Ext.onReady(function(){
+ Cufon.replace('h1', { fontFamily: 'cl-reg'});
+ Cufon.replace('h2', { fontFamily: 'cl-reg_it'});
+});
+
