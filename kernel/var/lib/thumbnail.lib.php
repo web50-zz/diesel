4b86e7123374843e2bc8d51be5ec8c3dad8fb3e0
Anthon S Litvinenko <a.litvinenko@web50.ru>
2011-03-07 15:46:35 +0300

    Added new thumbnail library

--- kernel/var/lib/thumbnail.lib.php
+++ kernel/var/lib/thumbnail.lib.php
@@ -32,7 +32,7 @@ class thumbnail
 		$this->source_file = $source;
 		$this->source_ext = strtolower(array_pop(preg_split('/\./', $source)));
 		$this->path_to_storage = $path_to_storage;
-		list($this->source_size['width'], $this->source_size['height']) = getimagesize($source);
+		list($this->source_size['width'], $this->source_size['height']) = getimagesize($this->path_to_storage . $source);
 	}
 
 	public function create($width, $height, $file_name = false)
@@ -41,22 +41,22 @@ class thumbnail
 		if (!$file_name)
 			$file_name = md5($this->source_file . mktime()) . '.' . $this->source_ext;
 
-		dbg::write(array('width' => $width, 'height' => $height, 'file_name' => $file_name));
-		dbg::write(array_merge($this->source_size, array('file_name' => $this->source_file)));
+		//dbg::write(array('width' => $width, 'height' => $height, 'file_name' => $file_name));
+		//dbg::write(array_merge($this->source_size, array('file_name' => $this->source_file)));
 		switch($this->source_ext)
 		{
 			case 'gif':
-				$source = imagecreatefromgif($this->source_file);
+				$source = imagecreatefromgif($this->path_to_storage . $this->source_file);
 				imagecopyresampled($image, $source, 0, 0, 0, 0, $width, $height, $this->source_size['width'], $this->source_size['height']);
 				$result = imagegif($image, $this->path_to_storage . $file_name);
 			break;
 			case 'jpeg': case 'jpg':
-				$source = imagecreatefromjpeg($this->source_file);
+				$source = imagecreatefromjpeg($this->path_to_storage . $this->source_file);
 				imagecopyresampled($image, $source, 0, 0, 0, 0, $width, $height, $this->source_size['width'], $this->source_size['height']);
 				$result = imagejpeg($image, $this->path_to_storage . $file_name, 100);
 			break;
 			case 'png':
-				$source = imagecreatefrompng($this->source_file);
+				$source = imagecreatefrompng($this->path_to_storage . $this->source_file);
 				imagecopyresampled($image, $source, 0, 0, 0, 0, $width, $height, $this->source_size['width'], $this->source_size['height']);
 				$result = imagepng($image, $this->path_to_storage . $file_name, 100);
 			break;
