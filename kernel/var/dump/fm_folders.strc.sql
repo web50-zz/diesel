a16c492ad546a2c23eae986b1302ea0d455d7023
Fedot B Pozdnyakov 9* <9@u9.ru>
2011-03-26 17:55:18 +0300

    kernel db dump

--- kernel/var/dump/fm_folders.strc.sql
+++ kernel/var/dump/fm_folders.strc.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `fm_folders` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL DEFAULT '',
-  `left` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `right` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `level` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
+  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `title` varchar(255) NOT NULL default '',
+  `left` mediumint(8) unsigned NOT NULL default '0',
+  `right` mediumint(8) unsigned NOT NULL default '0',
+  `level` mediumint(8) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`),
   KEY `left` (`left`,`right`,`level`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8
