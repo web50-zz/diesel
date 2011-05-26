155d2e2255d2af4967351f3694330f7ae1871942
Anthon S Litvinenko <a.litvinenko@web50.ru>
2011-05-26 21:43:01 +0400

    Added mailer library

--- kernel/etc/init/kernel.init.php
+++ kernel/etc/init/kernel.init.php
@@ -164,4 +164,7 @@ if (file_exists($lc_file))
 if (!function_exists('json_encode'))
 	if (!class_exists('Services_JSON'))
 		throw new Exception(LC::get_err('json_not_supported'));
+
+// NOTE: Include Switft Mail library
+include_once(LIB_PATH . 'Swift/swift_required.php');
 ?>
