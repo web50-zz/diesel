b9911cbd6c0f3c23fc95536ed8f3e838b963e0dd
Anthon S Litvinenko <a.litvinenko@web50.ru>
2012-12-12 17:52:08 +0400

    Added the JSON error handler into the registry. And rewrited null registry key exception.

--- kernel/var/lib/registry.lib.php
+++ kernel/var/lib/registry.lib.php
@@ -17,6 +17,8 @@ class registry
 			self::$storage[$name] = $di->get($name);
 		}
 		$rec = self::$storage[$name];
+		// If empty registry key return null
+		if (empty($rec)) return null;
 		
 		switch ($rec->type)
 		{
@@ -24,7 +26,32 @@ class registry
 				return (string)$rec->value;
 			break;
 			case 2:
-				return json_decode($rec->value);
+				$data = json_decode($rec->value, true);
+				switch (json_last_error())
+				{
+					case JSON_ERROR_NONE:
+						return $data;
+					break;
+					case JSON_ERROR_DEPTH:
+						dbg::write($rec->value . ' - Maximum stack depth exceeded');
+					break;
+					case JSON_ERROR_STATE_MISMATCH:
+						dbg::write($rec->value . ' - Underflow or the modes mismatch');
+					break;
+					case JSON_ERROR_CTRL_CHAR:
+						dbg::write($rec->value . ' - Unexpected control character found');
+					break;
+					case JSON_ERROR_SYNTAX:
+						dbg::write($rec->value . ' - Syntax error, malformed JSON');
+					break;
+					case JSON_ERROR_UTF8:
+						dbg::write($rec->value . ' - Malformed UTF-8 characters, possibly incorrectly encoded');
+					break;
+					default:
+						dbg::write($rec->value . ' - Unknown error');
+					break;
+				}
+				return false;
 			break;
 			case 3:
 				$cfg = $rec->value;
@@ -44,8 +71,9 @@ class registry
 				}
 			break;
 			default:
-				return (string)$rec->value; //9* пусть стринг по дефолту, да и не будет ругаться если нет ключа.
-				//throw new Exception("Unknown registry record type for '{$name}'");
+				//return (string)$rec->value; //9* пусть стринг по дефолту, да и не будет ругаться если нет ключа.
+				// A.Litvinenko: Это не проверка на наличие ключа, это проверка на тип ключа.
+				throw new Exception("Unknown registry record type for '{$name}'");
 		}
 	}
 }
