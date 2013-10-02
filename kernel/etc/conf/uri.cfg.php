092c57d57925773acdfd14a90f4bfed677c99283
Anthon S Litvinenko <a.litvinenko@wen50.ru>
2013-10-02 17:04:00 +0400

    Added new configuration file for URI

---
+++ kernel/etc/conf/uri.cfg.php
@@ -0,0 +1,35 @@
+<?php
+/**
+*	Конфигурирование обработчика URI 
+*
+* @author	Anthon S. Litvinenko <a.litvinenko@web50.ru>
+* @access	public
+* @package	SBIN Diesel
+* @since	2013-10-01
+*/
+$uri_configuration = array(
+	'/xxx\/.*/' => BASE_PATH . 'admin.php',
+	//'/.*/' => INSTANCES_PATH . 'wwwcore/www_handler.php',
+);
+/*
+
+RewriteRule	^xxx/$				adm_gui.php?ui=administrate&cll=workspace	[L,QSA]
+RewriteRule	^xxx/(.*)/$			adm_$1.php					[L,QSA]
+RewriteRule	^xxx/ui/([^/]+)/(.*)\.[a-z]+$	adm_gui.php?ui=$1&cll=$2			[L,QSA]
+RewriteRule	^xxx/di/([^/]+)/(.*)\.[a-z]+$	adm_data.php?di=$1&cll=$2			[L,QSA]
+
+RewriteRule	^files/$			file.php?di=fm_files&cll=get			[L,QSA]
+RewriteRule	^ui/([^/]+)/(.*)\.[a-z]+$	pub_gui.php?ui=$1&cll=$2			[L,QSA]
+# 9* 26032012 all cfg.php files
+RewriteRule     (.*).cfg.php$	index.php		
+# 9* 26032012 all files in etc
+RewriteRule     ^etc/(.*)	index.php		
+# 9* 26032012 all php files in instances
+RewriteRule     ^instances/(.*)\.php	index.php	
+# 9* 26032012 all directories in instances 
+RewriteRule     ^instances/(.*)/$	index.php	
+RewriteCond	%{REQUEST_FILENAME} !-f
+RewriteCond	%{REQUEST_FILENAME} !-d
+RewriteRule	^(.*)		index.php
+*/
+?>
